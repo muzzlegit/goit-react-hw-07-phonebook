b98c929902eef50d590b921d72096b2648277064
Роман <muzzlestore@gmail.com>
2023-05-19 22:12:55 +0300

    hw done

---
+++ src/components/ContactItem/ContactItem.styled.js
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+import theme from 'theme';
+
+export const Item = styled.li(
+  {
+    fontSize: '28px',
+    display: 'flex',
+    gap: '20px',
+  },
+  props => ({
+    color: props.color,
+  })
+);
+export const Button = styled.button(
+  {
+    border: 'none',
+    backgroundColor: 'transparent',
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  props => ({
+    color: props.color ? theme.colors.pGray : theme.colors.red,
+  })
+);
